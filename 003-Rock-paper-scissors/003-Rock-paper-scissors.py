c66fbc53f78f506800d7bd4704ae3bc457b0ca18
Thyro-30 <rohitkanneganty@gmail.com>
2025-06-18 06:35:35 -0700

    Add 003-Rock-paper-Scissors project

---
+++ 003-Rock-paper-scissors/003-Rock-paper-scissors.py
@@ -0,0 +1,22 @@
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors:\n ")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user ==computer:
+        return "It's a tie! You both chose " + user + "."
+    
+    if is_win(user, computer):
+        return "You win! " + user + " beats " + computer + "."
+    
+    if is_win(computer, user):
+        return "You lose! " + computer + " beats " + user + "."
+    
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or \
+            (player == 'p' and opponent == 'r') or \
+            (player == 's' and opponent == 'p'):
+        return True
+    
+print(play())
