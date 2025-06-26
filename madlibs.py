70fde7cbc895488683300dc0cda80a14d25c624c
Thyro-30 <rohitkanneganty@gmail.com>
2025-06-25 22:55:53 -0500

    remove madlibs.py

--- madlibs.py
+++
@@ -1,6 +0,0 @@
-adj = input ("Enter an adjective: ")
-verb1 = input ("Enter a verb: ")
-verb2 = input ("Enter another verb: ")
-famous_person = input ("Enter a famous person: ")
-madlib = f"Computer programming is so {adj}! It makes me want to {verb1} all day and {verb2} like {famous_person}."
-print(madlib)
